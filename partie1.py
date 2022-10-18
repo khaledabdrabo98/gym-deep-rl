aa1e86b600e0247253ebb639a3d94bbbc0506c98
BRIGNONE JEAN p1709655 <jean.brignone@etu.univ-lyon1.fr>
2022-10-18 14:55:26 +0000

    Update partie1.py

--- partie1.py
+++ partie1.py
@@ -1,15 +1,40 @@
 import gym
+import matplotlib.pyplot as plt
 
-
-env = gym.make("CartPole-v1", render_mode="human")
+env = gym.make('CartPole-v1', render_mode="human")
 env.action_space.seed(42)
 
 observation, info = env.reset(seed=42)
 
-for _ in range(1000):
-    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+def run(nb_ep):
+    tab_score = []
+    for e in range(nb_ep):
+        score = 0
+        for i in range(100):
 
-    if terminated or truncated:
-        observation, info = env.reset()
+            observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
 
-env.close()
+            if terminated or truncated:
+                observation, info = env.reset()
+                break
+
+            score=score+reward
+
+        tab_score.append(score)
+        print(e)
+        print(score)
+    return tab_score
+
+episodes = 10
+score = run(episodes)
+
+
+print(score)
+plt.plot([i+1 for i in range(0, episodes)], score)
+plt.xlabel('Episode no.')
+plt.ylabel('Score')
+#plt.show()
+
+
+
+env.close() 
