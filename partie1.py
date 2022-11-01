1e979379474ef4a007753593e2852f543000e69a
khaledabdrabo98 <khaledm@rocketmail.com>
2022-11-01 21:47:24 +0100

    Fix Random strat plot and video recorder

--- partie1.py
+++ partie1.py
@@ -1,4 +1,5 @@
 import gym
+from gym.wrappers import RecordVideo
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,7 +11,7 @@ import numpy as np
 def random_search(env, episodes):
     """ Random search strategy implementation."""
     l_rewards = []
-    for _ in range(episodes):
+    for i in range(episodes):
         state = env.reset()
         done = False
         total = 0
@@ -21,6 +22,8 @@ def random_search(env, episodes):
             observation, reward, done, _, _ = env.step(action)
             # Update total reward
             total += reward
+            # render environment (needed to record episodes)
+            # env.render()
             if done:
                 break
         # Add total to the list of rewards
@@ -28,11 +31,12 @@ def random_search(env, episodes):
     return l_rewards
 
 # Create and show plot 
-def create_plot(values, n_episodes):   
+def create_plot(title, values, n_episodes):   
     ''' Plot the reward curve and histogram of results over time.'''    
     f, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,5))
-    f.suptitle('Random Strategy')
+    f.suptitle(title)
     ax[0].plot(values, label='score per run')
+    ax[0].axhline(195, c='red', ls='--', label='goal')
     ax[0].set_xlabel('Episodes')
     ax[0].set_ylabel('Reward')
     x = range(len(values))
@@ -45,24 +49,24 @@ def create_plot(values, n_episodes):
         print('')
     
     ax[1].hist(values[-n_episodes:])
+    ax[1].axvline(195, c='red', label='goal')
     ax[1].set_xlabel("Scores last " + str(n_episodes) + " episodes")
     ax[1].set_ylabel('Frequency')
     plt.show()
 
 
 # MAIN
-env = gym.make("CartPole-v1", render_mode="human")
+env_id = "CartPole-v1"
+path_video = './episodes/random_agent/'
+plot_title = 'Random Strategy'
+n_episodes = 150
+
+# render_mode="human" -> visualize pygame window 
+env = gym.make(env_id, render_mode="rgb_array")
+env = RecordVideo(env, path_video, episode_trigger=lambda x: x % 25 == 0, name_prefix=format(env_id))
 env.action_space.seed(42)
 
-observation, info = env.reset(seed=42)
-n_episodes = 10
-
 print("Starting with", n_episodes, "episodes ...")
- 
-rewards = []
-for i in range(n_episodes):    
-    total = random_search(env, n_episodes)
-    print("Finished episode", i+1)
-    rewards = rewards + total
 
-create_plot(rewards, n_episodes)
+total = random_search(env, n_episodes)
+create_plot(plot_title, total, n_episodes)
